7f4c6f19d8e1a810258c2fa52c8e2abcef383080
billchuan <1767618766@qq.com>
2019-11-18 14:59:44 +0800

    src/com/gzeic/test/Main.java

---
+++ src/com/gzeic/test/Main.java
@@ -0,0 +1,14 @@
+package com.gzeic.test;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+
+	public class student {
+		int num;
+		int grade;
+		String name;
+		String age;
+	}
+}
