b712fdcd5565f8326dcacd77cdcc158d4d5723dd
billchuan <1767618766@qq.com>
2019-12-05 20:19:16 +0800

    更新

--- src/cn/test/first/JdbcTest.java
+++ src/cn/test/first/JdbcTest.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
 
-public class Tset {
+public class JdbcTest {
 	public static void main(String[] args) throws SQLException {
 		Statement stmt = null;
 		ResultSet rs = null;
